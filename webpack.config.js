3a1185b45d6454a57de4416aef3e5f6339f303e7
joe <terma95@gmail.com>
2017-07-23 15:12:56 +0500

    Added all devops boilerplate and simple todo app for test

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+webpack = require('webpack')
+path = require('path')
+HtmlWebpackPlugin = require('html-webpack-plugin')
+CleanWebpackPlugin = require('clean-webpack-plugin')
+CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        'ghviewer': './src/index.js',
+        //'ghviewer.min': './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new webpack.optimize.UglifyJsPlugin({include: /\.min\.js$/}),
+        new HtmlWebpackPlugin({template: 'src/index.ejs'}),
+        new CopyWebpackPlugin([{from: 'src/styles', to: 'styles'}])
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    }
+};
